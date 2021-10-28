47c4edb90bafdc3cacee6f6929da889e487e0222
jonnaliesel <jonnaliesel@gmail.com>
2021-10-28 12:29:48 +0200

    remove octagon code and math import

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-
 def cube():
     while True:
         try:
@@ -9,12 +7,6 @@ def cube():
             break
         except NameError:
             print("Please insert number values, for example 10, 25.5 , 100 etc.")
-
-def octagon_area():
-    side = eval(input(f'\nInsert octagon side length in centimeters '))
-    area = 2*side*2*(1+math.sqrt(2))
-
-    return f'\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nThe area of your octagon is {int(area)} cm^2\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 
 #Här under hittar ni era namn där ni ska ange funktionen ni vill anropa som ni får definiera här ovanför.
 #ändra choice text strängen med er valda objekt.
