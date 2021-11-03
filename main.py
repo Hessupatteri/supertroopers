04c1a21b97c8f1a31a3a040201e611e3366d03f3
Hessupatteri <jani.karhula@chasacademy.se>
2021-11-03 10:49:26 +0100

    simplified main.py file

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-def cube():
-    while True:
-        try:
-            cubeside = eval(input("Insert cube side length in meters:"))
-            cubevolume = cubeside ** 3
-            print(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nThe volume of the cube is {cubevolume}m^3\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            break
-        except NameError:
-            print("Please insert number values, for example 10, 25.5 , 100 etc.")
-
-def octagon_area():
-    side = eval(input(f'\nInsert octagon side length in centimeters '))
-    area = 2*side*2*(1+math.sqrt(2))
-
-    return f'\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nThe area of your octagon is {int(area)} cm^2\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-
-def sphere(): 
-    radius =float(input('Please submit the radius of the sphere in centimetres: '))
-    calculation=4*math.pi*(math.pow(radius, 2))
-    print (f'The area of the sphere is {calculation:1.2f}, cm\u00b2.')
+import math
 
 #Här under hittar ni era namn där ni ska ange funktionen ni vill anropa som ni får definiera här ovanför.
 #ändra choice text strängen med er valda objekt.
