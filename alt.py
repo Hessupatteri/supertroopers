6c3b2217c93d2346da828d703c4e06342730e636
Alva Thunberg <alvathunberg@icloud.com>
2021-11-03 10:44:37 +0100

    Edited

--- alt.py
+++ alt.py
@@ -1,9 +1,7 @@
 import math 
 
 def sphere(): 
+    print('Hello, this app will help you calculate the area of a sphere.')
     radius =float(input('Please submit the radius of the sphere: '))
     calculation=4*math.pi*(math.pow(radius, 2))
     print (f'The area of the sphere is {calculation:1.2f}, cm\u00b2.')
-
-print('Hello, this app will help you calculate the area of a sphere.')
-sphere()
